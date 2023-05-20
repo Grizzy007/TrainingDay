89d9359c06477b09aa242cc684df461e7a1dc3e8
Difenter <afonking.denis13@gmail.com>
2023-05-20 22:02:48 +0300

    feat: add new icons

--- client/src/components/Icons/index.jsx
+++ client/src/components/Icons/index.jsx
@@ -1,7 +1,7 @@
-import IconEntry from './IconEntry';
-import IconUserNav from './IconUserNav';
+import IconEntry from "./IconEntry";
+import IconUserNav from "./IconUserNav";
+import IconFacebook from "./IconFacebook";
+import IconTwitter from "./IconTwitter";
+import IconInstagram from "./IconInstagram";
 
-export {
-  IconEntry,
-  IconUserNav,
-};
+export { IconEntry, IconUserNav, IconFacebook, IconTwitter, IconInstagram };
