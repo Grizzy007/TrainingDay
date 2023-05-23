455f9739c5b07a68be142c17ace89b2eb00c19d0
Difenter <afonking.denis13@gmail.com>
2023-05-23 23:24:59 +0300

    refactor: add new icons

--- client/src/components/Icons/index.jsx
+++ client/src/components/Icons/index.jsx
@@ -3,5 +3,17 @@ import IconUserNav from "./IconUserNav";
 import IconFacebook from "./IconFacebook";
 import IconTwitter from "./IconTwitter";
 import IconInstagram from "./IconInstagram";
+import IconUserProfile from "./IconUserProfile";
+import IconChevronDown from "./IconChevronDown";
+import IconCalendar from "./IconCalendar";
 
-export { IconEntry, IconUserNav, IconFacebook, IconTwitter, IconInstagram };
+export {
+  IconEntry,
+  IconUserNav,
+  IconFacebook,
+  IconTwitter,
+  IconInstagram,
+  IconUserProfile,
+  IconChevronDown,
+  IconCalendar,
+};
