c24e92324d4648ac52c215fef8c8107a8b64ebce
Difenter <afonking.denis13@gmail.com>
2023-06-13 13:48:43 +0300

    refactor

--- client/src/hooks/programAPI.js
+++ client/src/hooks/programAPI.js
@@ -1,6 +1,6 @@
 import { $authHost } from "./index";
 
 export const newProgram = async (program) => {
-  const response = await $authHost.patch("/suggest-new-program", program);
+  const response = await $authHost.post("/suggest-new-program", program);
   return response;
 };
